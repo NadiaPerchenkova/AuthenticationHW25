31809b7b7a124bb02074d722ce999862d7279cf3
Nadia Perchenkova <perecnadya@gmail.com>
2020-10-28 15:18:41 +0300

    Initial Commit

---
+++ Sources/Authentication/Authentication.swift
@@ -0,0 +1,45 @@
+import Foundation
+import SwiftKeychainWrapper
+
+enum AuthError: Error {
+    case notExist
+    case alreadyExist
+    case wrongPassword
+    case somethingWentWrong
+}
+
+class Authentication {
+    
+    static let shared = Authentication()
+    private init() {}
+    
+    
+    func logIn(email: String, password: String) -> Result<Bool, AuthError> {
+        
+        let passwordKeycain: String? = KeychainWrapper.standard.string(forKey: email)
+        
+        if passwordKeycain == nil {
+            return .failure(.notExist)
+        } else if passwordKeycain != password {
+            return .failure(.wrongPassword)
+        }
+        return .success(true)
+    }
+    
+    func signUp(email: String, password: String) -> Result<Bool, AuthError> {
+        
+        let passwordKeycain: String? = KeychainWrapper.standard.string(forKey: email)
+        
+        if passwordKeycain == password {
+            return .failure(.alreadyExist)
+        }
+        
+        let saveSuccessful: Bool = KeychainWrapper.standard.set(password, forKey: email)
+        
+        if !saveSuccessful {
+            return .failure(.somethingWentWrong)
+        }
+        
+        return .success(true)
+    }
+}
